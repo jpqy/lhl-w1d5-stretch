a9b8f59503f20345af0aeeb40d7d447f064a4487
Jimmy P <29219382+jpqy@users.noreply.github.com>
2020-03-06 17:27:23 -0500

    implemented output logger

--- popBottles.js
+++ popBottles.js
@@ -1,37 +1,57 @@
 // Write a program so that you can figure out how many total bottles
 // of pop can be redeemed given a customer investment.
-const totalPop = (dollars, emptyBottles = 0, caps = 0) => {
+const printTotalPop = (dollars) => {
+  let earnedByEmptiesTotal = 0;
+  let earnedByCapsTotal = 0;
+  let remainingEmpties;
+  let remainingCaps;
+  const totalPop = (dollars, emptyBottles = 0, caps = 0) => {
 
-  // Not enough dollars or trades to get another bottle
-  if (dollars < 2 && (emptyBottles < 2 || !emptyBottles) && (caps < 4 || !caps)) {
-    return 0;
+    // Not enough dollars or trades to get another bottle, log the current
+    // supply to remind customer
+    if (dollars < 2 && (emptyBottles < 2 || !emptyBottles) && (caps < 4 || !caps)) {
+      remainingEmpties = emptyBottles;
+      remainingCaps = caps;
+      return 0;
+    }
+
+    // Buy new bottles with dollars
+    let newBottles = parseInt(dollars / 2);
+
+    // Trade empties for new bottles and reduce empties supply
+    if (emptyBottles >= 2) {
+      const earnedByEmpties = parseInt(emptyBottles / 2);
+      earnedByEmptiesTotal += earnedByEmpties;
+      newBottles += earnedByEmpties;
+      emptyBottles -= earnedByEmpties * 2;
+    }
+
+    // Trade bottle caps for new bottles and reduce caps supply
+    if (caps >= 4) {
+      const earnedByCaps = parseInt(caps / 4);
+      earnedByCapsTotal += earnedByCaps;
+      newBottles += earnedByCaps;
+      caps -= earnedByCaps * 4;
+    }
+
+    // Convert new bottles into emptys and caps
+    emptyBottles += newBottles;
+    caps += newBottles;
+
+    // Return how many new bottles obtained, and call function recursively
+    // to trade empties and caps
+    return newBottles + totalPop(0, emptyBottles, caps);
   }
-
-  // Buy new bottles with dollars
-  let newBottles = parseInt(dollars / 2);
-
-  // Trade empties for new bottles and reduce empties supply
-  if (emptyBottles >= 2) {
-    newBottles += parseInt(emptyBottles / 2);
-    emptyBottles -= parseInt(emptyBottles / 2) * 2;
-  }
-
-  // Trade bottle caps for new bottles and reduce caps supply
-  if (caps >= 4) {
-    newBottles += parseInt(caps / 4);
-    caps -= parseInt(caps / 4) * 4;
-  }
-
-  // Convert new bottles into emptys and caps
-  emptyBottles += newBottles;
-  caps += newBottles;
-
-  // Return how many new bottles obtained, and call function recursively
-  // to trade empties and caps
-  return newBottles + totalPop(0, emptyBottles, caps);
+  console.log(`
+  TOTAL BOTTLES: ${totalPop(dollars)}
+  REMAINING BOTTLES: ${remainingEmpties}
+  REMAINING CAPS: ${remainingCaps}
+  TOTAL EARNED:
+    BOTTLES: ${earnedByEmptiesTotal}
+    CAPS: ${earnedByCapsTotal}
+  `);
 }
 
-console.log(totalPop(10));
-console.log(totalPop(20));
-console.log(totalPop(30));
-console.log(totalPop(40));
+printTotalPop(10);
+printTotalPop(20);
+printTotalPop(40);
